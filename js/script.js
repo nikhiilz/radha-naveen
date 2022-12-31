ae5efc8776860e89bcbe0318119b3bd4f2890b4c
Arun <jangirarun786@gmail.com>
2022-12-31 16:57:21 +0300

    updated

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@
  */
 
 $(document).on('click', function(){
-    // document.getElementById("my_audio").play();
+    document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
 });
 
